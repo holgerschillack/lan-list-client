8a9a7d013d15d768e354d01970a4cb8544b0fb36
Holger Schillack <hs@jochenlinnemann.de>
2021-11-21 17:15:03 +0100

    Initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.vue"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
